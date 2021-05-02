ed93319dc33c44aef186f2149afd88d89ef6891a
RafaelChen1991 <46599960+RafaelChen1991@users.noreply.github.com>
2021-05-02 09:15:48 +0800

    Update hw3.js

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,9 +1,9 @@
 function reverse(str) {
-    var ns =""
+    var newStr =""
     for (var i = str.length-1; i >= 0; i--){
-        ns += str[i]
+        newStr += str[i]
     }
-    console.log(ns)  
+    console.log(newStr)  
 }
 
 reverse('hello');
