23935fddd504153274912bb48cc81fe151fc1742
RafaelChen1991 <46599960+RafaelChen1991@users.noreply.github.com>
2021-05-02 09:18:52 +0800

    Update hw5.js

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,24 +1,21 @@
 function join(arr, concatStr) {
-    var ns = ""
+    var newStr = ""
     for (var i = 1; i < arr.length; i += 2){
         arr.splice(i, 0, concatStr)
     }
     for (var m = 0; m <= arr.length-1; m++){
-        ns += (arr[m])
-        } return ns
+        newStr += (arr[m])
+    } return newStr
 } 
 
 
 function repeat(str, times) {
-    var ns = ""
+    var newStr = ""
     for (var i = 0; i <= times; i++){
-        ns = ns + str
-    } return ns
+        newStr = newStr + str
+    } return newStr
 
 }
 
 console.log(join(['a'], '!'));
 console.log(repeat('a', 5));
-
-
-
